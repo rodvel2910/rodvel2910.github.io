26bd484c5fa2c74c9fd2cb7a3f4a1ff95d3eb512
Diego Rodríguez Veloza <rodvel2910@icloud.com>
2021-03-05 08:59:16 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Test - PHP</title>
+</head>
+<body>
+	<p>
+		¿Se ve el siguiente texto?
+	</p>
+	<p>
+		<?php
+			echo 'Gucci';
+		?>
+	</p>
+</body>
+</html>
