851c1cf8eacc49122bc381e1112f50f6d98ea460
Diego Rodríguez Veloza <desarrolloapp@GT.BOG>
2021-03-05 08:36:12 -0500

    Create test.php
    Test de funcionamiento básico de PHP

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Test - PHP</title>
+</head>
+<body>
+	<p>
+		Si puedo ver la siguiente línea es porque PHP funciona alojado en GitHub Pages
+	</p>
+	<p>
+		<?php
+			echo 'Funciono';
+		?>
+	</p>
+</body>
+</html>
