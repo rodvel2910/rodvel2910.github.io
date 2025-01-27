2abc874d3abc521e352c015552320c9d395c5246
Diego Rodríguez Veloza <rodvel2910@icloud.com>
2025-01-27 17:50:33 -0500

    Commit inicial de estructura

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var btns_nav = document.getElementsByClassName('btn-nav')
+var pestanas = document.getElementsByClassName('wrapper-pestana')
+for (var i = 0; i < btns_nav.length; i++) {
+    btns_nav[i].addEventListener('click', function () {
+        cambiarPestana(this, this.getAttribute('data-pestana'))
+    })
+}
+function cambiarPestana(btn, pestana) {
+    for (var i = 0; i < btns_nav.length; i++) {
+        btns_nav[i].classList.remove('active')
+    }
+    for (var i = 0; i < pestanas.length; i++) {
+        pestanas[i].classList.add('hide')
+    }
+    btn.classList.add('active')
+    document.getElementById('pestana_' + pestana).classList.remove('hide')
+    console.log('Pestaña recibida: ' + pestana)
+}
