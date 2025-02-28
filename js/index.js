c8236c457bb4cb21602a3b3197a6a3888f5bae71
Diego Rodríguez Veloza <rodvel2910@icloud.com>
2025-02-28 12:12:19 -0500

    Corrección animaciones

--- js/index.js
+++ js/index.js
@@ -23,51 +23,61 @@ $(document).ready(function() {
         $('#titulo_saludo').removeClass('rebote')
         $('#titulo_saludo').addClass('desvanecer')
         setTimeout(() => {
-            $('#titulo_saludo').html('¡Bienvenido a mi portafolio!')
+            $('#titulo_saludo').html('Soy Diego Rodríguez Veloza.')
             $('#titulo_saludo').removeClass('desvanecer')
             $('#titulo_saludo').addClass('aparecer')
             setTimeout(() => {
                 $('#titulo_saludo').removeClass('aparecer')
                 $('#titulo_saludo').addClass('desvanecer')
                 setTimeout(() => {
-                    $('#wrapper_foto').css({
-                        'bottom': '0',
-                        'transform': 'translateY(-100%)'
-                    })
+                    $('#titulo_saludo').html('Y este es mi portafolio.')
+                    $('#titulo_saludo').removeClass('desvanecer')
+                    $('#titulo_saludo').addClass('aparecer')
                     setTimeout(() => {
-                        // $('#wrapper_foto').css({
-                        //     'top': '-18px',
-                        //     'transform': 'none',
-                        //     'transition': 'none'
-                        // })
+                        $('#titulo_saludo').removeClass('aparecer')
+                        $('#titulo_saludo').addClass('desvanecer')
                         setTimeout(() => {
                             $('#wrapper_foto').css({
-                                'transition': 'all .5s',
-                            })
-                            $('#wrapper_foto').css({
-                                'box-shadow': '0 0 40px #D4AF37',
+                                'opacity': '1',
+                                'transform': 'translateY(0px)',
+                                'transition': 'all .5s'
                             })
                             setTimeout(() => {
-                                $('#wrapper_foto').css({
-                                    'bottom': 'calc(100% - 350px)',
-                                })
-                                setTimeout(() => {
+                                // $('#wrapper_foto').css({
+                                //     'top': '-18px',
+                                //     'transform': 'none',
+                                //     'transition': 'none'
+                                // })
+                                /*setTimeout(() => {
                                     $('#wrapper_foto').css({
-                                        'transition': 'none',
+                                        'transition': 'all .5s',
                                     })
                                     $('#wrapper_foto').css({
-                                        'position': 'relative',
-                                        'top': '40px',
-                                    })
-                                    $('#wrapper_contenido').css({
-                                        'transition': 'all .5s'
+                                        'box-shadow': '0 0 40px #D4AF37',
                                     })
                                     setTimeout(() => {
-                                        $('#wrapper_contenido').css({
-                                            'opacity': '1'
+                                        $('#wrapper_foto').css({
+                                            'bottom': 'calc(100% - 350px)',
                                         })
-                                    }, 500)
-                                }, 500)
+                                        setTimeout(() => {
+                                            $('#wrapper_foto').css({
+                                                'transition': 'none',
+                                            })
+                                            $('#wrapper_foto').css({
+                                                'position': 'relative',
+                                                'top': '40px',
+                                            })
+                                            $('#wrapper_contenido').css({
+                                                'transition': 'all .5s'
+                                            })
+                                            setTimeout(() => {
+                                                $('#wrapper_contenido').css({
+                                                    'opacity': '1'
+                                                })
+                                            }, 500)
+                                        }, 500)
+                                    }, 1000)
+                                }, 500)*/
                             }, 1000)
                         }, 500)
                     }, 1000)
