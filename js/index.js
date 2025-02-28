b45c4db57625cca5bbf1af7cf3040ce9b742c7b5
Diego Rodríguez Veloza <rodvel2910@icloud.com>
2025-02-27 22:37:43 -0500

    Ajustes caché, animaciones y estructura

--- js/index.js
+++ js/index.js
@@ -19,8 +19,46 @@
 $(document).ready(function() {
     $('#titulo_saludo').addClass('rebote')
     setTimeout(() => {
-        // $('#titulo_saludo').parent().addClass('d-none')
+        // $('#titulo_saludo').addClass('d-none')
         $('#titulo_saludo').removeClass('rebote')
         $('#titulo_saludo').addClass('desvanecer')
+        setTimeout(() => {
+            $('#titulo_saludo').html('¡Bienvenido a mi portafolio!')
+            $('#titulo_saludo').removeClass('desvanecer')
+            $('#titulo_saludo').addClass('aparecer')
+            setTimeout(() => {
+                $('#titulo_saludo').removeClass('aparecer')
+                $('#titulo_saludo').addClass('desvanecer')
+                setTimeout(() => {
+                    $('#wrapper_foto').css({
+                        'bottom': '0',
+                        'transform': 'translateY(-100%)'
+                    })
+                    setTimeout(() => {
+                        $('#wrapper_foto').css({
+                            'top': '-18px',
+                            'transform': 'none',
+                            'transition': 'none'
+                        })
+                        setTimeout(() => {
+                            $('#wrapper_foto').css({
+                                'transition': 'all .5s',
+                            })
+                            $('#wrapper_foto').css({
+                                'box-shadow': '0 0 40px #D4AF37',
+                            })
+                            setTimeout(() => {
+                                $('#wrapper_foto').css({
+                                    'bottom': 'calc(100% - 350px)',
+                                })
+                                $('#wrapper_contenido').css({
+                                    'bottom': 'calc(100% - 425px)'
+                                })
+                            }, 1000)
+                        }, 500)
+                    }, 1000)
+                }, 500)
+            }, 2000)
+        }, 500)
     }, 2000)
 })
